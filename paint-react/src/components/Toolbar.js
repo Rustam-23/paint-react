cd0a8070efb22810129ca2d8224b84346b72eac7
Rustam Izmaylov <rizmailov@briogroup.ru>
2022-11-07 18:34:16 +0300

    added mobx, store, tool - brush

--- paint-react/src/components/Toolbar.js
+++ paint-react/src/components/Toolbar.js
@@ -1,10 +1,21 @@
 ﻿import React from "react";
-import '../styles/toolbar.scss';
+import "../styles/toolbar.scss";
+import { BsBrush, BsCircle, BsEraser } from "react-icons/bs";
+import { BiRectangle, BiRedo, BiSave, BiUndo } from "react-icons/bi";
+import { HiOutlineMinusSm } from "react-icons/hi";
 
 const Toolbar = () => {
   return (
     <div className="toolbar">
-      <button className="toolbar__button brush"></button>
+      <button className="toolbar__button brush"><BsBrush /></button>
+      <button className="toolbar__button rect"><BiRectangle /></button>
+      <button className="toolbar__button circle"><BsCircle /></button>
+      <button className="toolbar__button eraser"><BsEraser /></button>
+      <button className="toolbar__button line"><HiOutlineMinusSm /></button>
+      <input type="color" style={{ marginLeft: "10px"}}/>
+      <button className="toolbar__button undo"><BiUndo /></button>
+      <button className="toolbar__button redo"><BiRedo /></button>
+      <button className="toolbar__button save"><BiSave /></button>
     </div>
   );
 };
---
+++ paint-react/src/store/toolState.js
@@ -0,0 +1,14 @@
+﻿import { makeAutoObservable } from "mobx";
+
+class ToolState {
+  tool = null
+  constructor() {
+    makeAutoObservable(this)
+  }
+  
+  setTool(tool) {
+    this.tool = tool
+  }
+}
+
+export default new ToolState()
