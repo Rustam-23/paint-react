6a0d3d13da297fb596195c5557e52981212891cd
Rustam Izmaylov <rizmailov@briogroup.ru>
2022-11-08 17:59:19 +0300

    added circle, line

--- paint-react/src/components/Toolbar.js
+++ paint-react/src/components/Toolbar.js
@@ -8,15 +8,17 @@ import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
 import Rect from "../tools/Rect";
 import Eraser from "../tools/Eraser";
+import Circle from "../tools/Circle";
+import Line from "../tools/Line";
 
 const Toolbar = () => {
   return (
     <div className="toolbar">
       <button className="toolbar__button brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}><BsBrush /></button>
       <button className="toolbar__button rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}><BiRectangle /></button>
-      <button className="toolbar__button circle"><BsCircle /></button>
+      <button className="toolbar__button circle" onClick={() => toolState.setTool(new Circle(canvasState.canvas))}><BsCircle /></button>
       <button className="toolbar__button eraser" onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}><BsEraser /></button>
-      <button className="toolbar__button line"><HiOutlineMinusSm /></button>
+      <button className="toolbar__button line" onClick={() => toolState.setTool(new Line(canvasState.canvas))}><HiOutlineMinusSm /></button>
       <input type="color" style={{ marginLeft: "10px"}}/>
       <button className="toolbar__button undo"><BiUndo /></button>
       <button className="toolbar__button redo"><BiRedo /></button>
